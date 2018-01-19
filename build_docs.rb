9e6ddce828933a5616d1297fdbc76c8e81fcfab7
michaeljklein <michael14486@gmail.com>
2018-01-19 17:22:50 -0500

    fixed build_docs.rb, docs

--- build_docs.rb
+++ build_docs.rb
@@ -8,37 +8,27 @@ stack_haddock_stderr = Open3.capture3('stack haddock')[1]
 
 
 # Example: ".stack-work/install/x86_64-osx/lts-10.2/8.2.2/doc"
-built_docs_dir = stack_haddock_stderr.lines.select do |x|
-  x.match(/\.stack-work/)
-end.map do |x|
-  x.chomp.sub(/^.*\.stack-work/, './.stack-work')
-end.select do |x|
-  x.match(/\/doc\//)
-end.map do |x|
-  x.sub(/\/doc\/.*$/,'/doc')
+built_docs_dir = stack_haddock_stderr.lines.lazy.map do |x|
+  x.chomp.match(/(?<docs_dir>^.*\/\.stack-work\/install\/[^\/]+\/[^\/]+\/[^\/]+\/doc)\/index.html$/)
+end.select(&:itself).map do |x|
+  x[:docs_dir]
 end.first
 
 
+puts "Stack log:"
 puts stack_haddock_stderr
+puts
+puts "built_docs_dir: #{built_docs_dir}"
 
 
-# Copy built docs to 
+# Copy built docs to built_docs_dir
 if built_docs_dir && Dir.exist?(built_docs_dir)
-  puts "Built_docs_dir: #{built_docs_dir}"
-
   unless File.exist?('.gitignore')
     puts "No .gitignore in current directory, assuming you're not running this right and exiting"
     exit
   end
 
-  if Dir.exist?('doc/')
-    puts "Removing previous doc/ dir"
-    FileUtils.rm_rf 'doc/'
-  else
-    puts "No previous doc/ dir to remove"
-  end
-
-  FileUtils.cp_r built_docs_dir, 'doc/'
+  system "yes n | cp -r #{built_docs_dir} ."
   puts "Docs copied to doc/"
 else
   puts "Unable to find 'built_docs_dir', docs not copied to doc/"
